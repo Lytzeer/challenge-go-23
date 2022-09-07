5d7128aebbf504f6f4718c483b342f874f7ed60c
plukas <lukas.portier@ynov.com>
2022-09-07 20:59:56 +0200

    print combn

--- printcombn.go
+++ printcombn.go
@@ -60,19 +60,23 @@ func PrintCombN(n int) {
 			for j := '1'; j <= '7'; j++ {
 				for k := '2'; k <= '8'; k++ {
 					for l := '3'; l <= '9'; l++ {
-						if i == '6' && j == '7' && k == '8' && l == '9' {
-							z01.PrintRune(rune(i))
-							z01.PrintRune(rune(j))
-							z01.PrintRune(rune(k))
-							z01.PrintRune(rune(l))
-							z01.PrintRune('\n')
-						} else {
-							z01.PrintRune(rune(i))
-							z01.PrintRune(rune(j))
-							z01.PrintRune(rune(k))
-							z01.PrintRune(rune(l))
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
+						if i < j && j < k && k < l {
+							if i < j && j < k && k < l {
+								if i == '6' && j == '7' && k == '8' && l == '9' {
+									z01.PrintRune(rune(i))
+									z01.PrintRune(rune(j))
+									z01.PrintRune(rune(k))
+									z01.PrintRune(rune(l))
+									z01.PrintRune('\n')
+								} else {
+									z01.PrintRune(rune(i))
+									z01.PrintRune(rune(j))
+									z01.PrintRune(rune(k))
+									z01.PrintRune(rune(l))
+									z01.PrintRune(',')
+									z01.PrintRune(' ')
+								}
+							}
 						}
 					}
 				}
@@ -85,21 +89,23 @@ func PrintCombN(n int) {
 				for k := '2'; k <= '7'; k++ {
 					for l := '3'; l <= '8'; l++ {
 						for m := '4'; m <= '9'; m++ {
-							if i == '5' && j == '6' && k == '7' && l == '8' && m == '9' {
-								z01.PrintRune(rune(i))
-								z01.PrintRune(rune(j))
-								z01.PrintRune(rune(k))
-								z01.PrintRune(rune(l))
-								z01.PrintRune(rune(m))
-								z01.PrintRune('\n')
-							} else {
-								z01.PrintRune(rune(i))
-								z01.PrintRune(rune(j))
-								z01.PrintRune(rune(k))
-								z01.PrintRune(rune(l))
-								z01.PrintRune(rune(m))
-								z01.PrintRune(',')
-								z01.PrintRune(' ')
+							if i < j && j < k && k < l && l < m {
+								if i == '5' && j == '6' && k == '7' && l == '8' && m == '9' {
+									z01.PrintRune(rune(i))
+									z01.PrintRune(rune(j))
+									z01.PrintRune(rune(k))
+									z01.PrintRune(rune(l))
+									z01.PrintRune(rune(m))
+									z01.PrintRune('\n')
+								} else {
+									z01.PrintRune(rune(i))
+									z01.PrintRune(rune(j))
+									z01.PrintRune(rune(k))
+									z01.PrintRune(rune(l))
+									z01.PrintRune(rune(m))
+									z01.PrintRune(',')
+									z01.PrintRune(' ')
+								}
 							}
 						}
 					}
@@ -114,23 +120,25 @@ func PrintCombN(n int) {
 					for l := '3'; l <= '7'; l++ {
 						for m := '4'; m <= '8'; m++ {
 							for n := '5'; n <= '9'; n++ {
-								if i == '4' && j == '5' && k == '6' && l == '7' && m == '8' && n == '9' {
-									z01.PrintRune(rune(i))
-									z01.PrintRune(rune(j))
-									z01.PrintRune(rune(k))
-									z01.PrintRune(rune(l))
-									z01.PrintRune(rune(m))
-									z01.PrintRune(rune(n))
-									z01.PrintRune('\n')
-								} else {
-									z01.PrintRune(rune(i))
-									z01.PrintRune(rune(j))
-									z01.PrintRune(rune(k))
-									z01.PrintRune(rune(l))
-									z01.PrintRune(rune(m))
-									z01.PrintRune(rune(n))
-									z01.PrintRune(',')
-									z01.PrintRune(' ')
+								if i < j && j < k && k < l && l < m && m < n {
+									if i == '4' && j == '5' && k == '6' && l == '7' && m == '8' && n == '9' {
+										z01.PrintRune(rune(i))
+										z01.PrintRune(rune(j))
+										z01.PrintRune(rune(k))
+										z01.PrintRune(rune(l))
+										z01.PrintRune(rune(m))
+										z01.PrintRune(rune(n))
+										z01.PrintRune('\n')
+									} else {
+										z01.PrintRune(rune(i))
+										z01.PrintRune(rune(j))
+										z01.PrintRune(rune(k))
+										z01.PrintRune(rune(l))
+										z01.PrintRune(rune(m))
+										z01.PrintRune(rune(n))
+										z01.PrintRune(',')
+										z01.PrintRune(' ')
+									}
 								}
 							}
 						}
@@ -147,25 +155,27 @@ func PrintCombN(n int) {
 						for m := '4'; m <= '7'; m++ {
 							for n := '5'; n <= '8'; n++ {
 								for o := '6'; o <= '9'; o++ {
-									if i == '3' && j == '4' && k == '5' && l == '6' && m == '7' && n == '8' && o == '9' {
-										z01.PrintRune(rune(i))
-										z01.PrintRune(rune(j))
-										z01.PrintRune(rune(k))
-										z01.PrintRune(rune(l))
-										z01.PrintRune(rune(m))
-										z01.PrintRune(rune(n))
-										z01.PrintRune(rune(o))
-										z01.PrintRune('\n')
-									} else {
-										z01.PrintRune(rune(i))
-										z01.PrintRune(rune(j))
-										z01.PrintRune(rune(k))
-										z01.PrintRune(rune(l))
-										z01.PrintRune(rune(m))
-										z01.PrintRune(rune(n))
-										z01.PrintRune(rune(o))
-										z01.PrintRune(',')
-										z01.PrintRune(' ')
+									if i < j && j < k && k < l && l < m && m < n && n < o {
+										if i == '3' && j == '4' && k == '5' && l == '6' && m == '7' && n == '8' && o == '9' {
+											z01.PrintRune(rune(i))
+											z01.PrintRune(rune(j))
+											z01.PrintRune(rune(k))
+											z01.PrintRune(rune(l))
+											z01.PrintRune(rune(m))
+											z01.PrintRune(rune(n))
+											z01.PrintRune(rune(o))
+											z01.PrintRune('\n')
+										} else {
+											z01.PrintRune(rune(i))
+											z01.PrintRune(rune(j))
+											z01.PrintRune(rune(k))
+											z01.PrintRune(rune(l))
+											z01.PrintRune(rune(m))
+											z01.PrintRune(rune(n))
+											z01.PrintRune(rune(o))
+											z01.PrintRune(',')
+											z01.PrintRune(' ')
+										}
 									}
 								}
 							}
@@ -184,27 +194,29 @@ func PrintCombN(n int) {
 							for n := '5'; n <= '7'; n++ {
 								for o := '6'; o <= '8'; o++ {
 									for p := '7'; p <= '9'; p++ {
-										if i == '2' && j == '3' && k == '4' && l == '5' && m == '6' && n == '7' && o == '8' && p == '9' {
-											z01.PrintRune(rune(i))
-											z01.PrintRune(rune(j))
-											z01.PrintRune(rune(k))
-											z01.PrintRune(rune(l))
-											z01.PrintRune(rune(m))
-											z01.PrintRune(rune(n))
-											z01.PrintRune(rune(o))
-											z01.PrintRune(rune(p))
-											z01.PrintRune('\n')
-										} else {
-											z01.PrintRune(rune(i))
-											z01.PrintRune(rune(j))
-											z01.PrintRune(rune(k))
-											z01.PrintRune(rune(l))
-											z01.PrintRune(rune(m))
-											z01.PrintRune(rune(n))
-											z01.PrintRune(rune(o))
-											z01.PrintRune(rune(p))
-											z01.PrintRune(',')
-											z01.PrintRune(' ')
+										if i < j && j < k && k < l && l < m && m < n && n < o && o < p {
+											if i == '2' && j == '3' && k == '4' && l == '5' && m == '6' && n == '7' && o == '8' && p == '9' {
+												z01.PrintRune(rune(i))
+												z01.PrintRune(rune(j))
+												z01.PrintRune(rune(k))
+												z01.PrintRune(rune(l))
+												z01.PrintRune(rune(m))
+												z01.PrintRune(rune(n))
+												z01.PrintRune(rune(o))
+												z01.PrintRune(rune(p))
+												z01.PrintRune('\n')
+											} else {
+												z01.PrintRune(rune(i))
+												z01.PrintRune(rune(j))
+												z01.PrintRune(rune(k))
+												z01.PrintRune(rune(l))
+												z01.PrintRune(rune(m))
+												z01.PrintRune(rune(n))
+												z01.PrintRune(rune(o))
+												z01.PrintRune(rune(p))
+												z01.PrintRune(',')
+												z01.PrintRune(' ')
+											}
 										}
 									}
 								}
@@ -225,25 +237,27 @@ func PrintCombN(n int) {
 								for o := '6'; o <= '7'; o++ {
 									for p := '7'; p <= '8'; p++ {
 										for q := '8'; q <= '9'; q++ {
-											if i == '1' && j == '2' && k == '3' && l == '4' && m == '5' && n == '6' && o == '7' && p == '8' && q == '9' {
-												z01.PrintRune(rune(i))
-												z01.PrintRune(rune(j))
-												z01.PrintRune(rune(k))
-												z01.PrintRune(rune(l))
-												z01.PrintRune(rune(m))
-												z01.PrintRune(rune(n))
-												z01.PrintRune(rune(o))
-												z01.PrintRune('\n')
-											} else {
-												z01.PrintRune(rune(i))
-												z01.PrintRune(rune(j))
-												z01.PrintRune(rune(k))
-												z01.PrintRune(rune(l))
-												z01.PrintRune(rune(m))
-												z01.PrintRune(rune(n))
-												z01.PrintRune(rune(o))
-												z01.PrintRune(',')
-												z01.PrintRune(' ')
+											if i < j && j < k && k < l && l < m && m < n && n < o && o < p && p < q {
+												if i == '1' && j == '2' && k == '3' && l == '4' && m == '5' && n == '6' && o == '7' && p == '8' && q == '9' {
+													z01.PrintRune(rune(i))
+													z01.PrintRune(rune(j))
+													z01.PrintRune(rune(k))
+													z01.PrintRune(rune(l))
+													z01.PrintRune(rune(m))
+													z01.PrintRune(rune(n))
+													z01.PrintRune(rune(o))
+													z01.PrintRune('\n')
+												} else {
+													z01.PrintRune(rune(i))
+													z01.PrintRune(rune(j))
+													z01.PrintRune(rune(k))
+													z01.PrintRune(rune(l))
+													z01.PrintRune(rune(m))
+													z01.PrintRune(rune(n))
+													z01.PrintRune(rune(o))
+													z01.PrintRune(',')
+													z01.PrintRune(' ')
+												}
 											}
 										}
 									}
