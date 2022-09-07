c5500a23efd7ab1fd23b7891d60aaf64f31d249c
plukas <lukas.portier@ynov.com>
2022-09-07 15:36:09 +0200

    print comb 2

--- printcomb2.go
+++ printcomb2.go
@@ -4,12 +4,10 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j <= '9'; j++ {
+		for j := '0'; j <= '8'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
-					nb1 := i*10 + j
-					nb2 := k*10 + l
-					if nb1 != nb2 && nb1 < nb2 {
+					if string(i)+string(j) < string(k)+string(l) {
 						if i == '9' && j == '8' && k == '9' && l == '9' {
 							z01.PrintRune(rune(i))
 							z01.PrintRune(rune(j))
