0bd241b3a7e863c9174ffcccfcc5652ee04e7cfd
plukas <lukas.portier@ynov.com>
2022-09-07 17:13:33 +0200

    nbr

--- printnbr.go
+++ printnbr.go
@@ -1,5 +1,26 @@
 package piscine
 
+import (
+	"github.com/01-edu/z01"
+)
+
 func PrintNbr(n int) {
-	print(n)
+	if n < 0 {
+		z01.PrintRune('-')
+		n = n * -1
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	numberSlice := []rune{}
+	for n > 0 {
+		nb := n % 10
+		numberSlice = append(numberSlice, rune(nb+48))
+		n /= 10
+	}
+	for i := len(numberSlice) - 1; len(numberSlice) != 0; i-- {
+		z01.PrintRune(numberSlice[i])
+		numberSlice = numberSlice[:len(numberSlice)-1]
+	}
 }
