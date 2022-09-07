5d9cb497e8726a04977a5d5ddbbe66a1a31fe1a1
plukas <lukas.portier@ynov.com>
2022-09-07 17:18:39 +0200

    nbr

--- printnbr.go
+++ printnbr.go
@@ -5,6 +5,29 @@ import (
 )
 
 func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		z01.PrintRune('-')
+		z01.PrintRune('9')
+		z01.PrintRune('2')
+		z01.PrintRune('2')
+		z01.PrintRune('3')
+		z01.PrintRune('3')
+		z01.PrintRune('7')
+		z01.PrintRune('2')
+		z01.PrintRune('0')
+		z01.PrintRune('3')
+		z01.PrintRune('6')
+		z01.PrintRune('8')
+		z01.PrintRune('5')
+		z01.PrintRune('4')
+		z01.PrintRune('7')
+		z01.PrintRune('7')
+		z01.PrintRune('5')
+		z01.PrintRune('8')
+		z01.PrintRune('0')
+		z01.PrintRune('8')
+		return
+	}
 	if n < 0 {
 		z01.PrintRune('-')
 		n = n * -1
