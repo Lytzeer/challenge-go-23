67965590a6e041cef0e687abfa016adc13c2a3f5
plukas <lukas.portier@ynov.com>
2022-09-08 11:16:32 +0200

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,18 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	atoi := 0
+	n := 1
+	slice := []rune{}
+	for _, ch := range s {
+		if ch != '0' {
+			slice = append(slice, rune(ch))
+		}
+	}
+	for i := len(slice) - 1; len(slice) != 0; i-- {
+		atoi += int(slice[i]-48) * n
+		slice = slice[:len(slice)-1]
+		n *= 10
+	}
+	return atoi
+}
