748c849390fb60373fa408569b7ca01a8ed20f22
plukas <lukas.portier@ynov.com>
2022-09-12 10:54:41 +0200

    recursive factorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,14 +1,12 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 20 {
-		if nb == 1 {
-			return nb
-		} else if nb == 0 {
-			return 1
-		} else {
-			return RecursiveFactorial((nb - 1)) * nb
-		}
+	if nb == 1 {
+		return nb
+	} else if nb == 0 {
+		return 1
+	} else if nb < 20 {
+		return RecursiveFactorial((nb - 1)) * nb
 	} else {
 		return 0
 	}
