674a00ea75740e2f85696fad0f50a22e5ecf1cf9
plukas <lukas.portier@ynov.com>
2022-09-12 11:22:47 +0200

    recursive power

--- recursivepower.go
+++ recursivepower.go
@@ -4,9 +4,9 @@ func RecursivePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	} else if power < 0 {
-		return 1
-	} else if nb < 0 {
 		return 0
+	} else if nb == 0 && power == 0 {
+		return 1
 	} else {
 		return RecursivePower(nb, power-1) * nb
 	}
