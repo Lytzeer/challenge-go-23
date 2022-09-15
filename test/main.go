5ebf14c9b3b982a68b831bf915cdd94836276646
plukas <lukas.portier@ynov.com>
2022-09-15 09:54:04 +0200

    nrune

--- test/main.go
+++ test/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"piscine"
-
 	"github.com/01-edu/z01"
+
+	"piscine"
 )
 
 func main() {
-	z01.PrintRune(piscine.FirstRune("Hello!"))
-	z01.PrintRune(piscine.FirstRune("Salut!"))
-	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune(piscine.NRune("Hello!", 3))
+	z01.PrintRune(piscine.NRune("Salut!", 2))
+	z01.PrintRune(piscine.NRune("Bye!", -1))
+	z01.PrintRune(piscine.NRune("Bye!", 5))
+	z01.PrintRune(piscine.NRune("Ola!", 4))
 	z01.PrintRune('\n')
 }
