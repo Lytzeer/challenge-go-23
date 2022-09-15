9d0e335dc59861f203c31f92a228cc4f1abfed3b
plukas <lukas.portier@ynov.com>
2022-09-15 11:54:32 +0200

    is printable

--- isprintable.go
+++ isprintable.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrintable(s string) bool {
 	for _, ch := range s {
-		if rune(ch) < 33 || rune(ch) > 126 {
+		if rune(ch) < 32 || rune(ch) > 126 {
 			return false
 		}
 	}
