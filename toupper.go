e3f903127b5e634863e528e640d810eb068a4e3f
plukas <lukas.portier@ynov.com>
2022-09-15 12:00:59 +0200

    to upper

--- toupper.go
+++ toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	var word_upper string
 	for _, ch := range s {
 		if IsAlpha(string(ch)) {
-			word_upper += string(rune(ch - 22))
+			word_upper += string(rune(ch - 32))
 		} else {
 			word_upper += string(ch)
 		}
