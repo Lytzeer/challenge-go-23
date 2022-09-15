cf00b83773fe461e4db13467eb8aea4d84c421b5
plukas <lukas.portier@ynov.com>
2022-09-15 12:01:41 +0200

    to upper

--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) string {
 	var word_upper string
 	for _, ch := range s {
-		if IsAlpha(string(ch)) {
+		if IsLower(string(ch)) {
 			word_upper += string(rune(ch - 32))
 		} else {
 			word_upper += string(ch)
