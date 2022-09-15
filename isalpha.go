f90f106f6fd3dc2e7729c57f40ddbe7bac15337c
plukas <lukas.portier@ynov.com>
2022-09-15 11:50:26 +0200

    is alpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, ch := range s {
-		if !(IsUpper(string(ch))) && !(IsLower(string(ch))) && rune(ch) < 48 || rune(ch) > 57 {
+		if !(IsUpper(string(ch))) && !(IsLower(string(ch))) && (rune(ch) < 48 || rune(ch) > 57) {
 			return false
 		}
 	}
