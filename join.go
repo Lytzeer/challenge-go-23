cc8dae32d8afd22760ca676e3d4d5007b77268d4
plukas <lukas.portier@ynov.com>
2022-09-15 14:49:13 +0200

    join

--- join.go
+++ join.go
@@ -4,9 +4,9 @@ func Join(strs []string, sep string) string {
 	var word string
 	for i := 0; i < len(strs); i++ {
 		if i != len(strs)-1 {
-			word += strs[i]
-		} else {
 			word += strs[i] + sep
+		} else {
+			word += strs[i]
 		}
 	}
 	return word
