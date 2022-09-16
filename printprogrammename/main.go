33169341ef09f491519252d51ff6316a838677c0
plukas <lukas.portier@ynov.com>
2022-09-16 15:26:35 +0200

    checkpoint

---
+++ printprogrammename/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+	var index int
+	for i := len(arguments[0]) - 1; rune(arguments[0][i]) != rune(92); i-- {
+		index = i
+	}
+	for i := index; rune(arguments[0][i]) != rune(46); i++ {
+		z01.PrintRune(rune(arguments[0][i]))
+	}
+}
