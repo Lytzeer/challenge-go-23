7b6193f45a3c991256ff09616018fc3bd1b03551
plukas <lukas.portier@ynov.com>
2022-09-16 16:25:25 +0200

    print program name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,14 +7,10 @@ import (
 )
 
 func main() {
-	arguments := os.Args
-	var index int
-	for i := len(arguments[0]) - 1; rune(arguments[0][i]) != rune(92); i-- {
-		index = i
-	}
+	arguments := os.Args[0]
 	z01.PrintRune('.')
 	z01.PrintRune('/')
-	for i := index; rune(arguments[0][i]) != rune(46); i++ {
-		z01.PrintRune(rune(arguments[0][i]))
+	for _, ch := range arguments {
+		z01.PrintRune(ch)
 	}
 }
