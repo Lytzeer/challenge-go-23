9db7dd2fb9653e3baf9eba6093dfdefc61cfea5c
plukas <lukas.portier@ynov.com>
2022-09-20 09:22:29 +0200

    print words tables

--- printwordstables.go
+++ printwordstables.go
@@ -3,10 +3,10 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
-    for , mot := range a {
-        for , ch := range []rune(mot) {
-            z01.PrintRune(ch)
-        }
-        z01.PrintRune('\n')
-    }
+	for _, mot := range a {
+		for _, ch := range []rune(mot) {
+			z01.PrintRune(ch)
+		}
+		z01.PrintRune('\n')
+	}
 }
