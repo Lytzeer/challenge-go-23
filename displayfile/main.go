39339a141bb691539f2ca64ba75eb19400c83af6
plukas <lukas.portier@ynov.com>
2022-09-26 14:16:38 +0200

    display file

---
+++ displayfile/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	if len(args) == 0 {
+		fmt.Println("File name missing")
+	} else if len(args) > 1 {
+		fmt.Println("Too many arguments")
+	} else {
+		data, err := ioutil.ReadFile(args[1])
+		if err == nil {
+			fmt.Printf(string(data))
+		}
+	}
+}
