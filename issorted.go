e951591fc198b7f8bba0ceed12993b3cae321578
plukas <lukas.portier@ynov.com>
2022-09-26 15:37:18 +0200

    is sorted

--- issorted.go
+++ issorted.go
@@ -3,7 +3,7 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	if f(a[0], a[1]) > 0 {
 		for i := 1; i < len(a)-1; i++ {
-			if f(a[i], a[i+1]) > 0 {
+			if f(a[i], a[i+1]) < 0 {
 				return false
 			}
 		}
