acbbc596a8b3bd370ed8f44fc0f922cb593a0fa2
plukas <lukas.portier@ynov.com>
2022-09-26 15:18:19 +0200

    countif

--- countif.go
+++ countif.go
@@ -1,12 +1,15 @@
 package piscine
 
-rep := []bool{}
-	for i := 0; i < len(a); i++ {
-		rep = append(rep, f(a[i]))
+func CountIf(f func(string) bool, tab []string) int {
+	rep := []bool{}
+	for i := 0; i < len(tab); i++ {
+		rep = append(rep, f(tab[i]))
 	}
+	cpt := 0
 	for i := 0; i < len(rep); i++ {
 		if rep[i] {
-			return true
+			cpt += 1
 		}
 	}
-	return false
+	return cpt
+}
