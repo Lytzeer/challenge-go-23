069f7d12aea7138e22778c3d85ee6a5efe16acf3
plukas <lukas.portier@ynov.com>
2022-09-26 11:58:09 +0200

    point

---
+++ point/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+type point struct {
+	x, y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func Atoi(s string) int {
+	atoi := 0
+	for _, ch := range s {
+		if ch >= '0' && ch <= '9' {
+			atoi = atoi*10 + int(ch-'0')
+		} else {
+			return 0
+		}
+	}
+	return atoi
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+	x := string(points.x)
+	y := string(points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	for _, ch := range x {
+		z01.PrintRune(ch)
+	}
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	for _, ch := range y {
+		z01.PrintRune(ch)
+	}
+	z01.PrintRune('\n')
+}
