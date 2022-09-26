dcd0e06965afcf0a4065439fc0b6d115ce1f2468
plukas <lukas.portier@ynov.com>
2022-09-26 15:33:00 +0200

    is sorted

--- test/main.go
+++ test/main.go
@@ -6,6 +6,22 @@ import (
 )
 
 func main() {
-	test := []string{"Hello", "how", "are", "you?"}
-	fmt.Println(piscine.ConcatParams(test))
+	a1 := []int{0, 1, 2, 3, 4, 5}
+	a2 := []int{0, 2, 1, 3}
+
+	result1 := piscine.IsSorted(f, a1)
+	result2 := piscine.IsSorted(f, a2)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
+}
+
+func f(a, b int) int {
+	if a < b {
+		return 1
+	} else if a == b {
+		return 0
+	} else {
+		return -1
+	}
 }
