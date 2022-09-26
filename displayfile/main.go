af86e95a31293cb5d494452ba72ef67db0355d68
plukas <lukas.portier@ynov.com>
2022-09-26 14:17:41 +0200

    display file

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) == 0 {
+	if len(args) == 1 {
 		fmt.Println("File name missing")
-	} else if len(args) > 1 {
+	} else if len(args) >= 2 {
 		fmt.Println("Too many arguments")
 	} else {
 		data, err := ioutil.ReadFile(args[1])
