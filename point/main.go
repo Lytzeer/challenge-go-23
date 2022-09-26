b6c19e2ce3543eb5b88481db7c625a8f4b3c6371
plukas <lukas.portier@ynov.com>
2022-09-26 12:52:43 +0200

    point

--- point/main.go
+++ point/main.go
@@ -32,14 +32,14 @@ func main() {
 		tab_x = append(tab_x, ch)
 	}
 	for _, ch := range y {
-		tab_y = append(tab_y, ch)
+		tab_y = append(tab_y, rune(ch))
 	}
 	z01.PrintRune('x')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
 	for i := 0; i < len(tab_x); i++ {
-		z01.PrintRune(rune(tab_x[i]))
+		z01.PrintRune(tab_x[i])
 	}
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
@@ -48,7 +48,7 @@ func main() {
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
 	for i := 0; i < len(tab_y); i++ {
-		z01.PrintRune(rune(tab_y[i]))
+		z01.PrintRune(tab_y[i])
 	}
 	z01.PrintRune('\n')
 }
