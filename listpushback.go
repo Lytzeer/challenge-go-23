46778510a548b5a4614a2cc325d282dd70c997e1
plukas <lukas.portier@ynov.com>
2022-09-28 14:16:58 +0200

    List push back

--- listpushback.go
+++ listpushback.go
@@ -11,5 +11,12 @@ type List struct {
 }
 
 func ListPushBack(l *List, data interface{}) {
-
+	val := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = val
+		l.Tail = val
+	} else {
+		l.Tail.Next = val
+		l.Tail = val
+	}
 }
