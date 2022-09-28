8eb8f87fa82ed1b5fa3d7ebc6e5dc96f2b40f590
plukas <lukas.portier@ynov.com>
2022-09-28 14:25:34 +0200

    List push front

--- listpushfront.go
+++ listpushfront.go
@@ -3,8 +3,8 @@ package piscine
 func ListPushFront(l *List, data interface{}) {
 	val := &NodeL{Data: data}
 	if l.Tail == nil {
-		l.Tail = l.Head
 		l.Head = val
+		l.Head = l.Head
 	} else {
 		n := val
 		n.Next, l.Head = l.Head, n
