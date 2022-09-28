20bdd4b196617344fb6b08255f8a6b6387e057fb
plukas <lukas.portier@ynov.com>
2022-09-28 14:21:55 +0200

    List push front

---
+++ listpushfront.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ListPushFront(l *List, data interface{}) {
+	val := &NodeL{Data: data}
+	if l.Tail == nil {
+		l.Tail = l.Head
+		l.Head = val
+	} else {
+		val.Next = l.Head
+		l.Head = val
+	}
+}
